403fbbc7f0e228f62dcd673fbfaed05ef288e93f
Panadestein <rpana92@gmail.com>
2020-10-29 18:05:59 +0100

    Configured Org-mode

--- init.el
+++ init.el
@@ -49,6 +49,20 @@
 (tool-bar-mode -1)
 (show-paren-mode 1)
 
+;; Org-mode stuff
+
+(require 'org-tempo)
+(org-babel-do-load-languages
+ 'org-babel-load-languages '((fortran . t)
+			     (python . t)
+			     (C . t)
+			     (gnuplot . t)
+			     (awk . t)
+			     (latex . t)
+			     (shell . t)))
+
+(setq org-preview-latex-default-process 'imagemagick)
+
 ;; Spell checking with flyspell
 
 (require 'flyspell)
@@ -91,7 +105,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(powerline xclip spacemacs-theme auctex yasnippet-snippets ## elpy gruvbox-theme flycheck evil alect-themes)))
+   '(gnuplot powerline xclip spacemacs-theme auctex yasnippet-snippets ## elpy gruvbox-theme flycheck evil alect-themes)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

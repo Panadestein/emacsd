33d7468aed775a4136389970708851be41f9c638
Panadestein <rpana92@gmail.com>
2020-10-29 15:20:37 +0100

    More eye-candy stuff Emacs

--- init.el
+++ init.el
@@ -22,15 +22,32 @@
 ;; Some eye candy stuff (Sehr Wichtig!)
 
 (setq custom-safe-themes t)
-(load-theme 'gruvbox-dark-hard t)
+(load-theme 'spacemacs-dark t)
+
 (if (display-graphic-p)
     nil
-(set-face-attribute 'default nil :background "unspecified-bg"))
-(xterm-mouse-mode 1)
-(show-paren-mode 1)
+  (set-face-attribute 'default nil :background "unspecified-bg"))
+
+(require 'powerline)
+(powerline-default-theme)
+
 (when (version<= "26.0.50" emacs-version )
   (global-display-line-numbers-mode))
+
+;; Practical settings to make emacs more ergonomic
+;; Avoid enabling the xterm-mouse-mode option, as it
+;; introduces unwanted behaviour in the clipboard
+
+(if (fboundp #'save-place-mode)
+  (save-place-mode +1)
+  (setq-default save-place t))
+
 (setq inhibit-startup-screen t)
+
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+(show-paren-mode 1)
 
 ;; Spell checking with flyspell
 
@@ -57,7 +74,6 @@
 (setq c-default-style '((java-mode . "java")
                         (awk-mode . "awk")
                         (other . "linux")))
-
 ;; Snippets
 
 (require 'yasnippet)
@@ -75,7 +91,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(auctex yasnippet-snippets ## elpy gruvbox-theme flycheck evil alect-themes)))
+   '(powerline xclip spacemacs-theme auctex yasnippet-snippets ## elpy gruvbox-theme flycheck evil alect-themes)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

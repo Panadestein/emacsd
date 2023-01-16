c7575ae99479a5b3037058e54b76a0982ae608c7
Panadestein <rpana92@gmail.com>
2023-01-16 10:47:15 +0100

    Fix pipeline

--- build-site.el
+++ build-site.el
@@ -21,7 +21,6 @@
 ;; Install packages needed for HTML export
 
 (package-install 'htmlize)
-(package-install 'nix-mode)
 (package-install 'color-theme-modern)
 (package-install 'reformater)
 
@@ -29,7 +28,6 @@
 (require 'nix-mode)
 (require 'ox-publish)
 (require 'font-lock)
-(require 'reformatter)
 
 ;; Using this library is a work-around to get color in HTML exports.
 ;; Otherwise Emacs in batch mode cannot get the correct faces
